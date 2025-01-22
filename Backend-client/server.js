d649dab0bef637a664f5138458038217cfa2600b
Kaarlo Sasiang <sasiang64@gmail.com>
2025-01-23 03:54:39 +0800

    Update server.js

--- Backend-client/server.js
+++ Backend-client/server.js
@@ -41,6 +41,12 @@ const db = mysql.createConnection({
   },
 });
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_PORT);
+
 // Connect to MySQL database
 db.connect((err) => {
   if (err) {
