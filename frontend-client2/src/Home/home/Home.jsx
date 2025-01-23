ad57f2cb5b5efc7658e85b0e373e3822f92c718a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-23 11:11:16 +0800

    updated

--- frontend-client2/src/Home/home/Home.jsx
+++ frontend-client2/src/Home/home/Home.jsx
@@ -21,12 +21,6 @@ function Home() {
         <Sidebar isSidebarVisible={isSidebarVisible} />
       </div>
 
-      <div className="hometitlehome">
-        <h1 className="hometitlewelocome text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-800">
-          WELCOME TO BIOEXPLORER
-        </h1>
-      </div>
-
       <div className="cards">
         <Card />
       </div>
