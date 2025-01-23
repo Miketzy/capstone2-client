fd3fad71320ddf4bb364805304ba9d6148a1ba38
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-23 11:15:13 +0800

    updated

--- frontend-client2/src/Components/card/Card.jsx
+++ frontend-client2/src/Components/card/Card.jsx
@@ -6,7 +6,10 @@ function Card() {
   return (
     <div className="card">
       {/* Header Section */}
-      <header className="website-header text-center p-6 bg-blue-200 rounded-lg shadow-md mb-6">
+      <header className="website-header text-center p-6 bg-blue-200 rounded-lg shadow-md mb-6 mt-12">
+        <h1 className="text-2xl font-bold text-blue-800">
+          Welcome to BioExplorer
+        </h1>
         <p className="mt-2 text-gray-700">
           Explore the world of biodiversity through our interactive map,
           detailed database, engaging quizzes, and insightful analytics. This
