76057cfa519dddd85f02695d73e8b02d2c8ae1ff
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-24 22:15:20 +0800

    updated

--- frontend-client2/src/Components/frontpage/About-the-Project/AboutTheProject.jsx
+++ frontend-client2/src/Components/frontpage/About-the-Project/AboutTheProject.jsx
@@ -97,7 +97,7 @@ const AboutTheProject = () => {
                 <img
                   src="/picture/Screenshot 2025-01-24 220617.png"
                   alt="Search and Filter 2"
-                  className="absolute top-8 left-8 w-5/6 rounded-lg shadow-lg border-4 border-white"
+                  className="absolute top-20 left-8 w-5/6 rounded-lg shadow-lg border-4 border-white"
                 />
               </div>
               <p className="text-lg text-gray-700 bg-emerald-100 py-4 px-6 rounded-lg shadow-lg">
