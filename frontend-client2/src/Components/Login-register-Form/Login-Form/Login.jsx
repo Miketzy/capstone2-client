21d6c4d69e794e046ab1eb66a51bce3f0621ba23
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-25 20:17:03 +0800

    updated

--- frontend-client2/src/Components/Login-register-Form/Login-Form/Login.jsx
+++ frontend-client2/src/Components/Login-register-Form/Login-Form/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 
 const Login = () => {
   const [values, setValues] = useState({
@@ -75,12 +74,13 @@ const Login = () => {
       <div className="bg-white p-8 rounded-lg shadow-2xl w-full max-w-md">
         {/* Back Button */}
         <button
-          onClick={goBack}
+          onClick={() => (window.location.href = "/about-the-project")}
           className="absolute top-6 left-6 sm:top-4 sm:left-4 md:top-6 md:left-6 p-2 bg-green-600 rounded-full text-white hover:bg-green-700 transition duration-300 flex items-center justify-center"
           style={{ boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}
         >
           <KeyboardReturnIcon fontSize="large" />
         </button>
+
         {/* Updated Logo */}
         <div className="text-center mb-6">
           <img
