21d27ec3419043879c838933e2796f31bf041718
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-04-21 10:40:29 +0800

    updated

--- frontend-client2/src/Components/Analytics-page/Graph-page2/Graph2.jsx
+++ frontend-client2/src/Components/Analytics-page/Graph-page2/Graph2.jsx
@@ -41,7 +41,7 @@ function Graph2() {
 
   return (
     <div className="flex justify-center items-center my-10">
-      <div className="w-full max-w-5xl p-10 bg-white rounded-lg shadow-lg">
+      <div className="w-full max-w-4xl p-8 bg-white rounded-lg shadow-lg">
         <ResponsiveContainer width="100%" height={540}>
           <PieChart>
             <Pie
