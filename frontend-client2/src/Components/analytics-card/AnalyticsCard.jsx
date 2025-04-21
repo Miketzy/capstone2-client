96ef69cc67be456ab1b7975ef146e567cdecd368
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-04-21 09:56:37 +0800

    updated

--- frontend-client2/src/Components/analytics-card/AnalyticsCard.jsx
+++ frontend-client2/src/Components/analytics-card/AnalyticsCard.jsx
@@ -2,16 +2,12 @@ import React from "react";
 
 function AnalyticsCard() {
   return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <br />
+    <div className="flex items-center justify-center min-h-screen pt-24 bg-green-100">
       <div className="p-5">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
           {/* Card for Bar Chart Intro */}
           <div className="bg-blue-50 p-6 rounded-2xl shadow-md">
-            <h2 className="mb-4 text-xl font-semibold text-gray-800">
+            <h2 className="mb-4 text-xl font-semibold text-gray-800 border-b-2 border-teal-600 pb-2">
               Species Comparison (Bar Graph) - Introduction
             </h2>
             <p className="text-sm text-gray-600">
@@ -24,7 +20,7 @@ function AnalyticsCard() {
 
           {/* Card for Pie Chart Intro */}
           <div className="bg-blue-50 p-6 rounded-2xl shadow-md">
-            <h2 className="mb-4 text-xl font-semibold text-gray-800">
+            <h2 className="mb-4 text-xl font-semibold text-gray-800 border-b-2 border-teal-600 pb-2">
               Endangered Status (Pie Chart) - Introduction
             </h2>
             <p className="text-sm text-gray-600">
