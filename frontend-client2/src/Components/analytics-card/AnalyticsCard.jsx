271aad01de61b28c98bdee0379e82e5a88aff829
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-04-21 10:02:23 +0800

    updated

--- frontend-client2/src/Components/analytics-card/AnalyticsCard.jsx
+++ frontend-client2/src/Components/analytics-card/AnalyticsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AnalyticsCard() {
   return (
-    <div className="flex items-center justify-center min-h-screen pt-40">
+    <div className="flex items-center justify-center min-h-[80vh] pt-20">
       <div className="p-5">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
           {/* Card for Bar Chart Intro */}
