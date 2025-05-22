020079e6c5d2a08a1b3b8fe08f5c47f419a9077f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-05-22 11:16:06 +0800

    updated

--- frontend-client2/src/Components/Database/Database-image/Database.jsx
+++ frontend-client2/src/Components/Database/Database-image/Database.jsx
@@ -121,9 +121,11 @@ function Database() {
             <span className="close-icon" onClick={closeModal}>
               <CloseIcon />
             </span>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
-              Your Button Text
-            </button>
+            <div className="flex justify-end items-center gap-2 mb-4">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+                Your Button Text
+              </button>
+            </div>
             <div className="">
               {/* Main Image */}
               <img
